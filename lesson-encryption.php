bb745447c91a4addceb5b77ef481f3f785072cf4
Kelly Lougheed <kellylougheed@gmail.com>
2017-06-03 16:08:54 -0700

    Add additional materials

--- lesson-encryption.php
+++ lesson-encryption.php
@@ -8,8 +8,18 @@
 
 	<p>The goal of this lesson was to spark student interest about computer science by evoking the mysterious, high-stakes world of cybersecurity. Although cybersecurity and cryptography are complex fields, Caesar's Cipher provides an accessible introduction to core concepts in CS and security.</p>
 
-	<p>Below is my lesson plan.</p>
+	<p>Below is my lesson plan, followed by links to additional materials.</p>
 
 	<iframe class="scribd_iframe_embed" src="https://www.scribd.com/embeds/349713554/content?start_page=1&view_mode=scroll&access_key=key-huSLu3UrAGzkO7fwO0yV&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_61242" width="100%" height="600" frameborder="0"></iframe>
+
+	<br/><br/>
+
+	<p>
+		Additional materials:
+		<a href="https://www.scribd.com/document/350281060/Do-Now-Decode" target="_blank">Do Now worksheet</a>,
+		<a href="https://www.scribd.com/document/350281029/Encryption-Activity" target="_blank">encryption activity worksheet</a> (the teacher can fill in the numbers that correspond to the letters on the cipher to help students' thought process, or just leave the spaces blank so the students can fill in the shifted alphabet)
+	</p>
+
+	<p>The additional message to decrypt in the first activity is: AQW JCXG ETCEMGF VJG EQFG!</p>
 
 <?php print file_get_contents("footer.html")?>
--- lesson-latin-verb.php
+++ lesson-latin-verb.php
@@ -8,11 +8,16 @@
 
 	<p>In this lesson, I aimed to combine my rigorous approach to grammar with the excitement of games and spoken Latin. Students were motivated to learn the verb forms to communicate with their classmates and compete in the game.</p>
 
-	<p>Below is my lesson plan, followed by a picture of the whiteboard after a round of the game Columns.</p>
+	<p>Below is my lesson plan, followed by links to additional materials and a picture of the whiteboard after a round of the game Columns.</p>
 
 	<iframe class="scribd_iframe_embed" src="https://www.scribd.com/embeds/349714201/content?start_page=1&view_mode=scroll&access_key=key-LpMHBtkEamxTcGlNXO9H&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_94097" width="100%" height="600" frameborder="0"></iframe>
 
 	<br/><br/>
+
+	<p>Additional materials: 
+		<a href="https://www.scribd.com/document/350281089/Malo-Activity-Handout" target="_blank">malo activity handout</a>, 
+		<a href="https://www.scribd.com/document/350281114/Malo-Activity-Worksheet" target="_blank">malo activity worksheet</a>
+	</p>
 
 	<p>
 		The whiteboard after a finished game of Columns, as played by a Latin IB class reviewing indicative verbs:<br/>
