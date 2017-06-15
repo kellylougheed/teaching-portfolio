7bd6e52799b58eb0e188527e391071f320f5553b
Kelly Lougheed <kellylougheed@gmail.com>
2017-06-15 16:13:25 -0700

    Add Scratch activity

---
+++ activity-scratch.php
@@ -0,0 +1,11 @@
+<?php print file_get_contents("header.html")?>
+
+	<h1>Scratch Activity</h1>
+
+	<p>This activity is designed for coding students who have progressed at least to Unit 5 of the <a href="http://scratched.gse.harvard.edu/guide/files/CreativeComputing20141015.pdf" target="_blank">Scratch Curriculum Guide</a>. It was originally developed for a Computational Thinking &amp; Standards class at the College of St. Scholastica.</p>
+
+	<p>The activity engages students by encouraging them to build with Scratch the type of personality quiz they may have encountered before online or in magazines. It reinforces preexisting knowledge of variables and conditionals and expands student knowledge of logical operators and nested conditionals.</p>
+
+	<iframe class="scribd_iframe_embed" src="https://www.scribd.com/embeds/351412681/content?start_page=1&view_mode=scroll&access_key=key-kB8b2RfTiPiDEdGgFaDk&show_recommendations=true" data-auto-height="false" data-aspect-ratio="0.7729220222793488" scrolling="no" id="doc_11152" width="100%" height="600" frameborder="0"></iframe>
+
+<?php print file_get_contents("footer.html")?>
