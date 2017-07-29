cebcc748ff498e2e0fdcab31845dbd19802c16e7
Kelly Lougheed <kellylougheed@gmail.com>
2017-07-29 11:41:22 -0700

    Update experience section and add list

--- education.php
+++ education.php
@@ -8,16 +8,21 @@
 	<p>I've been making websites since 3rd grade and have also worked as a software developer. After completing a web developer internship at OmniUpdate, I accepted a developer position at Specialty Medical Drugstore, where I worked primarily with Python and JavaScript. I've completed a variety of coursework and professional development in software.</p>
 
 	<h3>College of St. Scholastica</h3>
-	<p>Starting summer 2017, I am pursuing a graduate certificate in Computer Science Education. The coursework covers the curricula of AP Computer Science A and AP Computer Science Principles using Java and MIT's App Inventor. The certificate emphasizes pedagogy and diversity.</p>
+	<p>Since summer 2017, I have been pursuing a graduate certificate in Computer Science Education. The coursework covers AP Computer Science Principles using MIT's App Inventor and the Mobile CSP curriculum, as well as the curriculum of AP Computer Science A using Java. The certificate includes work on computational thinking using Scratch, diversity and inclusion practices, and CS education policy.</p>
 
-	<h3>Flatiron School</h3>
-	<p>In April-May 2017, I completed a 125-hour course in iOS app development and Swift and built numerous iPhone applications.</p>
+	<h3>Professional Development</h3>
+	<p>I regularly participate in PD and complete online courses to stay up-to-date with the field and expand my computer science knowledge. My PD work includes:</p>
 
-	<h3>University of Texas-Austin</h3>
-	<p>In February-March 2017, I earned a certificate in Foundations of Computer Science for Teachers from UT Austin. The course covered core CS concepts such as data structures (linked lists, binary trees, graphs, stacks &amp; queues), searching &amp; sorting algorithms, big O notation, hardware, and networking.</p>
+	<ul>
+		<li> <strong>Cybersecurity</strong>: I am currently completing the NSA's Cyber Teacher Certificate as well as an introductory cybersecurity course through University of Washington on edX. (7/17-Present)</li>
+		<li> <strong>Beauty &amp; Joy of Computing</strong>: I completed 60 hours of training in UC Berkeley's Beauty &amp; Joy of Computing curriculum for AP Computer Science Principles, which uses the Snap! programming language. (7/17)</li>
+		<li> <strong>Swift: Building iOS Apps</strong>: I completed a 125-hour course in iOS app development and Swift at the Flatiron School. (4/17-5/17)</li>
+		<li> <strong>Internet History, Technology, &amp; Security</strong>: I completed a 10-week course on the history, technology, and security of the internet through University of Michigan on Coursera (2/17-4/17).</li>
+		<li> <strong>Computer Science Fundamentals for Teachers</strong>: I completed a six-week course on data structures and algorithms through University of Texas-Austin on edX (2/17-3/17).</li>
+	</ul>
 
 	<h3>Firehose Software Engineering Program</h3>
-	<p>From August 2016 - January 2017, I completed an online coding bootcamp which specialized in Ruby on Rails. I built six RoR web applications and worked in a remote agile team to build a RoR chess application. Using Ruby and JavaScript, the coursework also covered data structures, algorithms, and computer science fundamentals such as object-oriented programming, recursion, runtime, binary &amp; hexadecimal numbers, and public key encryption.</p>
+	<p>In 2016, I completed a six-month program in full-stack web development and computer science fundamentals. I built six web applications using Ruby on Rails, JavaScript, and HTML/CSS and worked on a remote agile team to build a RoR chess application. Using Ruby and JavaScript, the course also covered data structures and algorithms.</p>
 
 	<h3>Stanford University</h3>
 	<p>In summer 2007, I completed Stanford's Java-based intro computer science class as an rising high school junior.</p>
@@ -30,7 +35,7 @@
 	<p>In May 2013, I earned my B.A. in Classics (Latin &amp; Greek) magna cum laude. While at Brown, I led the Classics Department Undergraduate Group and promoted Classics with a number of new activities, such as a field trip to the MFA, an inscription walk, a graduate student panel, and a celebration of the Ides of March. I was awarded the Workman-Driscoll Premium for my service to the department. My honors thesis on the Tomb of Caecilia Metella combined literature, history, and archaeology and was awarded the James Aldrich Pirce Prize for best thesis on a Roman topic as well as the department's nomination for best thesis in the university.</p>
 
 	<h3>Living Latin in Rome</h3>
-	<p>In summer 2012, I participated in the Paideia Institute's Living Latin in Rome program, where I practiced conversational Latin in Rome, Italy. Upon returning to Brown in the fall, I co-founded Spoken Latin at Brown. The Paideia Institute invited me back the following summer as an Assistant Teacher for their high school spoken Latin program.</p>
+	<p>In summer 2012, I participated in the Paideia Institute's Living Latin in Rome program, where I practiced spoken Latin in Rome, Italy. Upon returning to Brown in the fall, I co-founded Spoken Latin at Brown. The Paideia Institute invited me back the following summer as an Assistant Teacher for their high school spoken Latin program.</p>
 
 	<h3>Intercollegiate Center for Classical Studies</h3>
 	<p>In fall 2011, I studied Roman history and archaeology at the Intercollegiate Center for Classical Studies. While completing coursework in Greek literature and Latin inscriptions, I traveled throughout Italy and Sicily to study the Roman ruins and interned at the American Academy in Rome.</p>
--- experience.php
+++ experience.php
@@ -5,7 +5,7 @@
 	<h2>Full-Time Teaching Positions</h2>
 
 	<h3>Wildwood School - Coding Teacher</h3>
-	<p>In fall 2017, I will begin the coding program in the middle and upper school. The middle school curriculum will use HTML/CSS and JavaScript, and an upper school elective will incorporate Ruby programming and Ruby on Rails.</p>
+	<p>In fall 2017, I will begin the coding program in the middle and upper school. The middle school curriculum will use HTML/CSS and JavaScript, and an upper school elective will include Ruby programming and Ruby on Rails.</p>
 
 	<h3>Episcopal School of Jacksonville - Latin Teacher</h3>
 	<p>From 2014 to 2016, I taught Latin I, IB, II, III, and IV to students in grades 8-12. I used games and activities to reinforce learning Latin grammar and vocabulary and collaborated with the other Latin teacher to develop a Roman culture curriculum. I ensured student success on assessments with "practice tests" and did projects on Greek mythology and Roman history, as well as a unit on the film Gladiator. As the JCL (Latin Club) advisor, I provided activities for meetings while encouraging students to take on club responsibilities. In summer 2016, I created academic materials for the school Ancient Italy trip and served as a chaperone.</p>
@@ -16,7 +16,7 @@
 	<h2>Other Teaching Experience</h2>
 
 	<h3>Girl Develop It - Los Angeles</h3>
-	<p>Since early 2017, I have volunteered as a teaching assistant for introductory programming classes in JavaScript, HTML/CSS, and PHP/MySQL. I assisted with in-class projects, answered questions about programming, and troubleshot technical issues.</p>
+	<p>In spring 2017, I volunteered as a teaching assistant for introductory programming classes in JavaScript, HTML/CSS, and PHP/MySQL. I assisted with in-class projects, answered questions about programming, and troubleshot technical issues.</p>
 
 	<h3>The Paideia Institute</h3>
 	<p>In summer 2013, I was an assistant teacher for Living Latin in Rome High School. In addition to chaperoning the students in the dorm and around the city, I led small groups in spoken Latin and translation activities around Rome.</p>
